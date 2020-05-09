1bb24b4c5f0e47ee3fb823ebc3a350aad1db8f4d
Daniel Hines <d4hines@gmail.com>
2020-05-09 14:18:13 -0400

    wip

---
+++ compiler/src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let alm_file = env::args()
+        .nth(1)
+        .expect("Please specify an ALM module to compile.");
+
+    let read_file_error = format!("Could not read file {}!", alm_file);
+    let contents = fs::read_to_string(alm_file).expect(read_file_error.as_str());
+
+    println!("{}", contents);
+}
