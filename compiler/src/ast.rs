db908089e07be24b6111d7951fbec67f7b1bf62c
Daniel Hines <d4hines@gmail.com>
2020-05-11 11:18:44 -0400

    wip

--- compiler/src/ast.rs
+++ compiler/src/ast.rs
@@ -1,33 +1,90 @@
+use std::fmt::{Debug, Error, Formatter};
+/////////////////////////////////////////////
+pub enum Expr {
+    Number(i32),
+    Op(Box<Expr>, Opcode, Box<Expr>),
+    Error,
+}
+
+pub enum ExprSymbol<'input> {
+    NumSymbol(&'input str),
+    Op(Box<ExprSymbol<'input>>, Opcode, Box<ExprSymbol<'input>>),
+    Error,
+}
+
+#[derive(Copy, Clone)]
+pub enum Opcode {
+    Mul,
+    Div,
+    Add,
+    Sub,
+}
+
+impl Debug for Expr {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::Expr::*;
+        match *self {
+            Number(n) => write!(fmt, "{:?}", n),
+            Op(ref l, op, ref r) => write!(fmt, "({:?} {:?} {:?})", l, op, r),
+            Error => write!(fmt, "error"),
+        }
+    }
+}
+
+impl<'input> Debug for ExprSymbol<'input> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::ExprSymbol::*;
+        match *self {
+            NumSymbol(n) => write!(fmt, "{:?}", n),
+            Op(ref l, op, ref r) => write!(fmt, "({:?} {:?} {:?})", l, op, r),
+            Error => write!(fmt, "error"),
+        }
+    }
+}
+
+impl Debug for Opcode {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::Opcode::*;
+        match *self {
+            Mul => write!(fmt, "*"),
+            Div => write!(fmt, "/"),
+            Add => write!(fmt, "+"),
+            Sub => write!(fmt, "-"),
+        }
+    }
+}
+
+//////////////////////////////////
 pub type UpperTerm = String;
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub enum Term {
     TermUpper(UpperTerm),
     TermInteger(i64),
     TermBoolean(bool),
 }
-
 pub type Variable = String;
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub enum Expression {
     ExpressionTerm(Term),
     ExpressionVariable(Variable),
 }
 
+#[derive(PartialEq, Debug)]
 pub struct Enum {
     pub name: UpperTerm,
     pub terms: Vec<UpperTerm>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct Sort {
     pub name: UpperTerm,
     pub parent_sorts: Vec<UpperTerm>,
     pub attributes: Option<FunctionDeclarations>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct FunctionDeclaration {
     pub name: UpperTerm,
     pub params: Option<Vec<UpperTerm>>,
@@ -35,7 +92,7 @@ pub struct FunctionDeclaration {
 }
 
 pub type FunctionDeclarations = Vec<FunctionDeclaration>;
-
+#[derive(PartialEq, Debug)]
 pub struct DefinedFluentDeclaration {
     pub output: bool,
     pub declaration: FunctionDeclaration,
@@ -43,11 +100,13 @@ pub struct DefinedFluentDeclaration {
 
 pub type DefinedFluentDeclarations = Vec<DefinedFluentDeclaration>;
 
+#[derive(PartialEq, Debug)]
 pub struct Fluents {
     pub basic: FunctionDeclarations,
     pub defined: DefinedFluentDeclarations,
 }
 
+#[derive(PartialEq, Debug)]
 pub struct FunctionAssignment {
     pub name: UpperTerm,
     pub arguments: Vec<Expression>,
@@ -55,7 +114,7 @@ pub struct FunctionAssignment {
     pub negated: bool,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub enum FunctionAssignmentKinds {
     InFluent,
     OutFluent,
@@ -64,11 +123,12 @@ pub enum FunctionAssignmentKinds {
 
 pub type RawDDLog = String;
 
+#[derive(PartialEq, Debug)]
 pub enum RuleClause {
     ClauseFunctionAssignment(FunctionAssignment),
     ClauseRawDDLog(RawDDLog),
 }
-
+#[derive(PartialEq, Debug)]
 pub enum Axiom {
     StaticAssignment {
         name: UpperTerm,
@@ -86,6 +146,7 @@ pub type Sorts = Vec<Sort>;
 pub type Statics = FunctionDeclarations;
 pub type Axioms = Vec<Axiom>;
 
+#[derive(PartialEq, Debug)]
 pub struct ALMModule {
     pub enums: Enums,
     pub sorts: Sorts,
--- compiler/src/parse.rs
+++ compiler/src/parse.rs
@@ -1,15 +1,79 @@
-lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+lalrpop_mod!(pub calculator); // synthesized by LALRPOP
+lalrpop_mod!(pub parse);
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::ast::*;
     use std::fs;
 
     #[test]
-    fn calculator1() {
-        assert!(calculator1::TermParser::new().parse("22").is_ok());
-        assert!(calculator1::TermParser::new().parse("(22)").is_ok());
-        assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
-        assert!(calculator1::TermParser::new().parse("((22)").is_err());
+    fn calculator5() {
+        let expr = calculator::ExprsParser::new().parse("").unwrap();
+        assert_eq!(&format!("{:?}", expr), "[]");
+
+        let expr = calculator::ExprsParser::new()
+            .parse("22 * 44 + 66")
+            .unwrap();
+        assert_eq!(&format!("{:?}", expr), "[((22 * 44) + 66)]");
+
+        let expr = calculator::ExprsParser::new()
+            .parse("22 * 44 + 66,")
+            .unwrap();
+        assert_eq!(&format!("{:?}", expr), "[((22 * 44) + 66)]");
+
+        let expr = calculator::ExprsParser::new()
+            .parse("22 * 44 + 66, 13*3")
+            .unwrap();
+        assert_eq!(&format!("{:?}", expr), "[((22 * 44) + 66), (13 * 3)]");
+
+        let expr = calculator::ExprsParser::new()
+            .parse("22 * 44 + 66, 13*3,")
+            .unwrap();
+        assert_eq!(&format!("{:?}", expr), "[((22 * 44) + 66), (13 * 3)]");
+    }
+
+    #[test]
+    fn foo() {
+        let module =
+            fs::read_to_string("./test/logic.alm").expect("Failed to read ./test/logic.alm");
+        let parsed = parse::ALMModuleParser::new().parse(module.as_str()).unwrap();
+        assert_eq!(
+            parsed,
+            ALMModule {
+                enums: vec![Enum {
+                    name: "Directions".to_string(),
+                    terms: vec![
+                        "DTop".to_string(),
+                        "DLeft".to_string(),
+                        "DBottom".to_string(),
+                        "DRight".to_string(),
+                    ],
+                }],
+                sorts: vec![
+                    Sort {
+                        name: "Rectangles".to_string(),
+                        parent_sorts: vec!["Universe".to_string()],
+                        attributes: Some(vec![
+                            FunctionDeclaration {
+                            name: "Width".to_string(),
+                            params: None,
+                            ret: "Integers".to_string(),
+                        },
+                        FunctionDeclaration {
+                            name: "Height".to_string(),
+                            params: None,
+                            ret: "Integers".to_string(),
+                        }])
+                    },
+                ],
+                statics: vec![],
+                fluents: Fluents {
+                    basic: vec![],
+                    defined: vec![],
+                },
+                axioms: vec![],
+            }
+        );
     }
 }
