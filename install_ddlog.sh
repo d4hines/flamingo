5849cec6d5c48a24e2b86db5855998c003a6b8a5
Daniel Hines <d4hines@gmail.com>
2020-05-11 22:37:37 -0400

    wip

--- install_ddlog.sh
+++ install_ddlog.sh
@@ -5,6 +5,7 @@ if ! echo ":$PATH:" | grep -q :"ddlog": ; then
     echo "Fetching DDLog..."
     wget -q https://github.com/vmware/differential-datalog/releases/download/v0.17.4/ddlog-v0.17.4-20200508014413-linux.tar.gz -O ddlog.tar.gz
     echo "Unpacking DDLog binary..."
+    rm -rf ddlog
     tar -xzf ddlog.tar.gz
     mv ddlog ~/.ddlog
     rm ddlog.tar.gz
@@ -12,6 +13,8 @@ if ! echo ":$PATH:" | grep -q :"ddlog": ; then
     echo "Adding DDLog to path."
     echo "export DDLOG_HOME=~/.ddlog" >> ~/.bashrc
     echo "export PATH=\$DDLOG_HOME/bin:\$PATH" >> ~/.bashrc
+    echo "export DDLOG_HOME=~/.ddlog" >> ~/.profile
+    echo "export PATH=\$DDLOG_HOME/bin:\$PATH" >> ~/.profile
     echo "Done"
 else
     echo "DDlog is already on path. Skipping."
